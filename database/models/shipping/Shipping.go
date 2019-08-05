c3a7393e53a90ea1b0e171d6f526de87b959e74b
Aditia Pratama <aditiapratamagg@gmail.com>
2019-08-05 23:50:20 +0700

    add relation between shipping and item entity

--- database/models/shipping/Shipping.go
+++ database/models/shipping/Shipping.go
@@ -1,9 +1,12 @@
 package models
 
+import itemModel "github.com/aditiapratama1231/adit-microservice/database/models/item"
+
 type Shipping struct {
 	Model
-	ShippingNo string `gorm:"shipping_no" json:"shipping_no"` 
-	CustomerNote string `gorm:"customer_note" json:"customer_note"`
-	Status string `gorm:"status" json:"status"`
-	ItemID int32 `gorm:"item_id" json:"item_id"`
+	ShippingNo   string           `gorm:"shipping_no" json:"shipping_no"`
+	CustomerNote string           `gorm:"customer_note" json:"customer_note"`
+	Status       string           `gorm:"status" json:"status"`
+	ItemID       int32            `gorm:"item_id" json:"item_id"`
+	Item         []itemModel.Item `json:"items"`
 }
